04cd4a47899cfb96ca3dea48f5953787d4a0ca25
1nsanov <73361511+1nsanov@users.noreply.github.com>
2021-10-28 20:51:19 +0300

    Добавьте файлы проекта.

---
+++ 28_10_21/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _28_10_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreeD A = new ThreeD(1, 2, 3);
+            ThreeD B = new ThreeD(10, 10, 10);
+            ThreeD C;
+            Console.Write("Координаты точки A: ");
+            A.Show();
+            Console.Write("Координаты точки B: ");
+            B.Show();
+
+            Console.Write("Результат A++: ");
+            A++;
+            A.Show();
+            Console.Write("Результат B--: ");
+            B--;
+
+            B.Show();
+            C = A + B; 
+            Console.Write("Результат сложения A + B: ");
+            C.Show();
+            C = A - B;
+            Console.Write("Результат вычитания A - B: ");
+            C.Show();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 28_10_21/ThreeD.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace _28_10_21
+{
+    public class ThreeD
+    {
+        private int x, y, z;
+
+        public ThreeD()
+        {
+            x = y = z = 0;
+        }
+
+        public ThreeD(int i, int j, int k)
+        {
+            x = i; y = j; z = k;
+        }
+
+        public static ThreeD operator +(ThreeD op1, ThreeD op2)
+        {
+            ThreeD result = new ThreeD();
+            result.x = op1.x + op2.x; 
+            result.y = op1.y + op2.y; 
+            result.z = op1.z + op2.z;
+            return result;
+        }
+        public static ThreeD operator -(ThreeD op1, ThreeD op2)
+        {
+            ThreeD result = new ThreeD();
+            result.x = op1.x - op2.x;
+            result.y = op1.y - op2.y;
+            result.z = op1.z - op2.z;
+            return result;
+        }
+
+        // ++
+        public static ThreeD operator ++(ThreeD op)
+        {
+            ThreeD result = new ThreeD();
+            result.x = op.x + 1;
+            result.y = op.y + 1;
+            result.z = op.z + 1;
+            return result;
+        }
+
+        // --
+        public static ThreeD operator --(ThreeD op)
+        {
+            ThreeD result = new ThreeD();
+            result.x = op.x - 1;
+            result.y = op.y - 1;
+            result.z = op.z - 1;
+            return result;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine(x + ", " + y + ", " + z);
+        }
+    }
+
+}
